468ebb979cc0bd14ccf40f46afa5c871c2654ecd
Đặng Nguyễn Minh An <dangnguyenminhan123@gmail.com>
2023-07-11 00:32:40 +0700

    dashboard

--- src/components/Admin/DashboardComponents/RevenueChart/RevenueChart.jsx
+++ src/components/Admin/DashboardComponents/RevenueChart/RevenueChart.jsx
@@ -1,17 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { AuthContext } from "../../../../context/authContext";
-import {
-    Chart as ChartJS,
-    LinearScale,
-    CategoryScale,
-    BarElement,
-    PointElement,
-    LineElement,
-    Legend,
-    Tooltip,
-    LineController,
-    BarController,
-} from 'chart.js';
+import { Chart as ChartJS, LinearScale, CategoryScale, BarElement, PointElement, LineElement, Legend, Tooltip, LineController, BarController } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
 import "./style.css";
 import { getBookingList } from '../../../../api/booking';
@@ -35,13 +24,15 @@ function RevenueChart() {
 
     const quarters = [1, 2, 3, 4];
     const months = [...Array(12)].map((_, i) => i + 1);
+    const years = [2022, 2023, 2024, 2025];
 
     const currentDate = new Date();
     const currentMonth = currentDate.getMonth() + 1;
     const currentQuarter = Math.floor((currentMonth + 2) / 3);
+    const currentYear = currentDate.getFullYear();
 
-    const dataRevenueHandle = reportType === 'quarter' ? (
-        quarters.map((quarter) => {
+    const dataRevenueHandle = reportType === 'quarter'
+        ? quarters.map((quarter) => {
             if (quarter <= currentQuarter) {
                 const bookingsInQuarter = dataBooking.filter((booking) => {
                     if (
@@ -63,30 +54,51 @@ function RevenueChart() {
             }
             return null;
         })
-    ) : (
-        months.map((month) => {
-            if (month <= currentMonth) {
-                const bookingsInMonth = dataBooking.filter((booking) => {
-                    if (
-                        (booking.bookingStatus === 'Delivered' ||
-                            booking.bookingStatus === 'Completed' ||
-                            booking.bookingStatus === 'Done') &&
-                        new Date(booking.createdAt).getMonth() + 1 === month &&
-                        booking.totalPrice
-                    ) {
-                        return true;
-                    }
-                    return false;
-                });
+        : reportType === 'month'
+            ? months.map((month) => {
+                if (month <= currentMonth) {
+                    const bookingsInMonth = dataBooking.filter((booking) => {
+                        if (
+                            (booking.bookingStatus === 'Delivered' ||
+                                booking.bookingStatus === 'Completed' ||
+                                booking.bookingStatus === 'Done') &&
+                            new Date(booking.createdAt).getMonth() + 1 === month &&
+                            booking.totalPrice
+                        ) {
+                            return true;
+                        }
+                        return false;
+                    });
 
-                const total = bookingsInMonth.reduce((acc, item) => {
-                    return acc + item.totalPrice;
-                }, 0);
-                return total;
-            }
-            return null;
-        })
-    );
+                    const total = bookingsInMonth.reduce((acc, item) => {
+                        return acc + item.totalPrice;
+                    }, 0);
+                    return total;
+                }
+                return null;
+            })
+            : years.map((year) => {
+                if (year <= currentYear ) {  // Thay "years" bằng "year"
+                    const bookingsInYear = dataBooking.filter((booking) => {
+                        if (
+                            (booking.bookingStatus === 'Delivered' ||
+                                booking.bookingStatus === 'Completed' ||
+                                booking.bookingStatus === 'Done') &&
+                            new Date(booking.createdAt).getFullYear() === parseInt(year) &&
+                            booking.totalPrice
+                        ) {
+                            return true;
+                        }
+                        return false;
+                    });
+
+                    const total = bookingsInYear.reduce((acc, item) => {
+                        return acc + item.totalPrice;
+                    }, 0);
+                    return total;
+                }
+                return null
+            });
 
     useEffect(() => {
         getBookingList(currentToken).then((res) => {
@@ -94,20 +106,24 @@ function RevenueChart() {
         });
     }, [currentToken]);
 
-    const labels = reportType === 'quarter' ? ['Q1', 'Q2', 'Q3', 'Q4'] : [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
+    const labels = reportType === 'quarter'
+        ? ['Q1', 'Q2', 'Q3', 'Q4']
+        : reportType === 'month'
+            ? [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            ]
+            : years.map(String);  // Sử dụng map để chuyển các giá trị trong mảng "years" thành chuỗi
 
     const data = {
         height: '100%',
@@ -127,7 +143,7 @@ function RevenueChart() {
         ],
     };
 
-    const option = {
+    const options = {
         responsive: true,
         maintainAspectRatio: false,
         plugins: {
@@ -141,11 +157,10 @@ function RevenueChart() {
         <div className='barchart d-flex flex-column justify-content-center gap-1'>
             <div className='d-flex justify-content-between'>
                 <h4>Business situation</h4>
-                <button className='btn btn-outline-secondary'>2023</button>
+                <button className='btn btn-outline-secondary'>{currentYear}</button>
             </div>
-
             <div className='h-85'>
-                <Chart type='bar' data={data} options={option} />
+                <Chart type='bar' data={data} options={options} />
             </div>
             <div className='d-flex justify-content-end gap-2 pt-1'>
                 <button
@@ -162,7 +177,11 @@ function RevenueChart() {
                 >
                     Month
                 </button>
-                <button type='button' className='btn btn-secondary'>
+                <button
+                    type='button'
+                    className={`btn ${reportType === 'year' ? 'btn-outline-secondary' : 'btn-secondary'}`}
+                    onClick={() => setReportType('year')}
+                >
                     Year
                 </button>
             </div>
